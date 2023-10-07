f654a752d3ec88c6135c8d2f3aa10f60562fc6dd
freearhey <7253922+freearhey@users.noreply.github.com>
2023-10-07 05:12:26 +0300

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
+}

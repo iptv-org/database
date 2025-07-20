82d60afda3b93960cb01e7bae9a55d31d424a696
freearhey <7253922+freearhey@users.noreply.github.com>
2025-07-20 04:30:56 +0300

    Update scripts

--- scripts/models/category.ts
+++ scripts/models/category.ts
@@ -5,18 +5,21 @@ import Joi from 'joi'
 export class Category extends Model {
   id: string
   name: string
+  description: string
 
   constructor(data: CategoryData) {
     super()
 
     this.id = data.id
     this.name = data.name
+    this.description = data.description
   }
 
   data(): CategoryData {
     return {
       id: this.id,
-      name: this.name
+      name: this.name,
+      description: this.description
     }
   }
 
@@ -27,7 +30,8 @@ export class Category extends Model {
         .required(),
       name: Joi.string()
         .regex(/^[A-Z]+$/i)
-        .required()
+        .required(),
+      description: Joi.string().required()
     })
   }
 }

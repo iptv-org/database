7066cf526d4ae2e2ae1f4ad23705e15e34ba0a25
freearhey <7253922+freearhey@users.noreply.github.com>
2025-08-19 17:47:43 +0300

    Update tests

--- tests/commands/db/export.test.ts
+++ tests/commands/db/export.test.ts
@@ -2,18 +2,14 @@ import { pathToFileURL } from 'node:url'
 import { execSync } from 'child_process'
 import * as fs from 'fs-extra'
 import { glob } from 'glob'
-import os from 'os'
 
 beforeEach(() => {
   fs.emptyDirSync('tests/__data__/output')
 })
 
 describe('db:export', () => {
-  let ENV_VAR = 'DATA_DIR=tests/__data__/input/db/export/data API_DIR=tests/__data__/output/api'
-  if (os.platform() === 'win32') {
-    ENV_VAR =
-      'SET "DATA_DIR=tests/__data__/input/db/export/data" && SET "API_DIR=tests/__data__/output/api" &&'
-  }
+  const ENV_VAR =
+    'cross-env DATA_DIR=tests/__data__/input/db/export/data API_DIR=tests/__data__/output/api'
 
   it('can export data as json', () => {
     const cmd = `${ENV_VAR} npm run db:export`
--- tests/commands/db/update.test.ts
+++ tests/commands/db/update.test.ts
@@ -1,6 +1,7 @@
+import { pathToFileURL } from 'node:url'
 import { execSync } from 'child_process'
 import * as fs from 'fs-extra'
-import os from 'os'
+import { glob } from 'glob'
 
 beforeEach(() => {
   fs.emptyDirSync('tests/__data__/output')
@@ -8,32 +9,34 @@ beforeEach(() => {
 })
 
 describe('db:update', () => {
-  let ENV_VAR = 'DATA_DIR=tests/__data__/output/data'
-  if (os.platform() === 'win32') {
-    ENV_VAR = 'SET "DATA_DIR=tests/__data__/output/data" &&'
-  }
+  const ENV_VAR = 'cross-env DATA_DIR=tests/__data__/output/data'
 
   it('can update db with data from issues', () => {
     const cmd = `${ENV_VAR} npm run db:update --silent`
     const stdout = execSync(cmd, { encoding: 'utf8' })
     if (process.env.DEBUG === 'true') console.log(cmd, stdout)
 
-    expect(content('output/data/blocklist.csv')).toEqual(
-      content('expected/db/update/data/blocklist.csv')
-    )
-    expect(content('output/data/channels.csv')).toEqual(
-      content('expected/db/update/data/channels.csv')
-    )
-    expect(content('output/data/feeds.csv')).toEqual(content('expected/db/update/data/feeds.csv'))
-    expect(content('output/data/logos.csv')).toEqual(content('expected/db/update/data/logos.csv'))
+    const files = glob.sync('tests/__data__/expected/db/update/data/*.csv').map(filepath => {
+      const fileUrl = pathToFileURL(filepath).toString()
+      const pathToRemove = pathToFileURL('tests/__data__/expected/db/update/data/').toString()
+
+      return fileUrl.replace(pathToRemove, '')
+    })
+
+    files.forEach(filepath => {
+      expect(content(`tests/__data__/output/data/${filepath}`), filepath).toBe(
+        content(`tests/__data__/expected/db/update/data/${filepath}`)
+      )
+    })
+
     expect(stdout).toEqual(
-      'OUTPUT=closes #9871, closes #9902, closes #9903, closes #9904, closes #9905, closes #9901, closes #6871, closes #7901, closes #17612, closes #9907, closes #8900, closes #9900, closes #5871, closes #5901, closes #5902, closes #5903, closes #5701, closes #9906, closes #5900, closes #5899, closes #5898, closes #5897, closes #5891'
+      'OUTPUT=closes #9871, closes #9902, closes #9903, closes #9904, closes #9905, closes #9901, closes #6871, closes #7901, closes #17612, closes #9907, closes #8900, closes #9900, closes #5871, closes #5901, closes #5902, closes #5903, closes #5701, closes #9906, closes #5900, closes #5899, closes #5898, closes #9912, closes #9911, closes #9910, closes #5897, closes #5891'
     )
   })
 })
 
 function content(filepath: string) {
-  return fs.readFileSync(`tests/__data__/${filepath}`, {
+  return fs.readFileSync(pathToFileURL(filepath), {
     encoding: 'utf8'
   })
 }
